01fb9c4a56417bdbe10ad52880986ebafff253cf
Panostam <tamt.pan@gmail.com>
2021-04-06 21:28:45 +0200

    commit

--- models/example/confirmed_cases.sql
+++ models/example/confirmed_cases.sql
@@ -1,6 +1,5 @@
 SELECT
-  SUM(cumulative_confirmed) AS total_cases_worldwide
+  SUM(cumulative_confirmed)
 FROM
   `bigquery-public-data.covid19_open_data.covid19_open_data`
-WHERE
-  date = "2020-04-15"
+
